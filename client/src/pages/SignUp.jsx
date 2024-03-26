6f1fe845740fe74d831f7f13cfd2f1efa58a58f0
Mahesh Giri <girimahesh.m@gmail.com>
2024-03-27 01:21:46 +0530

    add functionality signup page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,54 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignUp = () => {
+    const [formData, setFormData] = useState({});
+
+    const [loading, setLoading] = useState(false);
+
+    const [errorMessage, seterrorMessage] = useState(null);
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!formData.username || !formData.email || !formData.password) {
+            return seterrorMessage("Please fill all the fields");
+        }
+
+        try {
+            setLoading(true);
+            seterrorMessage(null);
+            const response = await fetch("/api/v1/auth/signup", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData),
+            });
+
+            const data = await response.json();
+
+            if (data.success === false) {
+                return seterrorMessage(data.message);
+            }
+            
+            setLoading(false);
+
+            if (response.ok) {
+                navigate("/sign-in")
+            }
+        } catch (error) {
+            seterrorMessage(error.message);
+            setLoading(false);
+        }
+    };
+
     return (
         <div className="min-h-screen mt-20">
             <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -14,30 +61,46 @@ const SignUp = () => {
                         Blog
                     </Link>
                     <p className="text-sm mt-5">
-                        This is a demo project. You can sign up with your email and password or with Google
+                        This is a mern-blog project. You can sign up with your email and password or with Google
                     </p>
                 </div>
                 {/* Right */}
                 <div className="flex-1">
-                    <form className="flex flex-col gap-4">
+                    <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
                         <div>
                             <Label value="Username" />
-                            <TextInput type="text" placeholder="Username" id="username" />
+                            <TextInput type="text" placeholder="Username" id="username" onChange={handleChange} />
                         </div>
                         <div>
                             <Label value="Email" />
-                            <TextInput type="text" placeholder="name@company.com" id="email" />
+                            <TextInput type="text" placeholder="name@company.com" id="email" onChange={handleChange} />
                         </div>
                         <div>
                             <Label value="Password" />
-                            <TextInput type="password" placeholder="Password" id="password" />
+                            <TextInput type="password" placeholder="Password" id="password" onChange={handleChange} />
                         </div>
-                        <Button gradientDuoTone="purpleToPink" type="submit">Sign Up</Button>
+                        <Button gradientDuoTone="purpleToPink" type="submit" disabled={loading}>
+                            {loading ? (
+                                <>
+                                    <Spinner size="sm" />
+                                    <span className="pl-3">Loading...</span>
+                                </>
+                            ) : (
+                                "Sign Up"
+                            )}
+                        </Button>
                     </form>
                     <div className="flex gap-2 text-sm mt-5">
                         <span>Have an account?</span>
-                        <Link to="/sign-in" className="text-blue-500">Sign In</Link>
+                        <Link to="/sign-in" className="text-blue-500">
+                            Sign In
+                        </Link>
                     </div>
+                    {errorMessage && (
+                        <Alert className="mt-5" color="failure">
+                            {errorMessage}
+                        </Alert>
+                    )}
                 </div>
             </div>
         </div>
