a5c8495c6a5ed15952aa3742b4c8f318bf351380
Mahesh Giri <girimahesh.m@gmail.com>
2024-03-28 00:34:55 +0530

    Create sign in API route

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use("/api/v1/auth", authRoute);
 
 // Middleware
 app.use((err, req, res, next) => {
-    const statusCode = err.status || 500;
+    const statusCode = err.statusCode || 500;
     const message = err.message || "Internal Server Error";
 
     res.status(statusCode).json({
