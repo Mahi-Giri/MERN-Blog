c164df6dbd5da19a5bcabd0a187a30c8d35f3935
Mahesh Giri <girimahesh.m@gmail.com>
2024-04-13 22:41:48 +0530

    create get post API route

--- server/routes/post.route.js
+++ server/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { verifyUser } from "../utils/verifyUser.js";
-import { createPost } from "../controllers/createPost.controller.js";
+import { createPost, getPost } from "../controllers/createPost.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyUser, createPost);
+router.get("/getPosts", getPost);
 
 export default router;
