d576b10678507f76cea3346800e7a4afd18c0bca
Mahesh Giri <girimahesh.m@gmail.com>
2024-04-27 14:41:48 +0530

    Change the port no to deploy

--- server/index.js
+++ server/index.js
@@ -23,8 +23,8 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.listen(8000, () => {
-    console.log("Server is running on port 8000...");
+app.listen(3000, () => {
+    console.log("Server is running on port 3000...");
 });
 
 app.use("/api/v1/user", userRoute);
